68bddf9fa145f97501b49fdb7b7dcdad86b23889
EkoRamadn <ekor4m4d4n1@gmail.com>
2024-10-26 12:43:12 +0700

    update stater webpack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,26 @@
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+
+
+// export default [
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+// ];
+
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        require: true,   // Tambahkan require sebagai global
+        module: true,    // Tambahkan module sebagai global
+        __dirname: true, // Tambahkan __dirname sebagai global
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
